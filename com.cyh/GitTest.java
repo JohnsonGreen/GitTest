54a05c8e58d62b96bb7a6142d21bec07efb35bec
陈恒 <chenheng003@ke.com>
2020-10-07 15:27:20 +0800

    第一次提交

---
+++ com.cyh/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author chenheng
+ * @date 2020-10-07 14:51
+ * @description
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
